01b25cea9512e99681a518d83dacb76d92ebd3f0
AhaduCoding <wasez1214@gmail.com>
2022-12-18 22:31:23 -0800

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,8 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * _puts - prints a string, followed by a new line.
  * @str: input string to print.
  */
-
 void _puts(char *str)
 {
 		while (*str != '\0')
